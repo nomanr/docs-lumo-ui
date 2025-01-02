33f22cd0df317d5ab9e2c45febad96865dd8f2c5
nomanr <nomrafique@gmail.com>
2025-01-02 15:01:45 +0000

    add components section

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,5 +1,5 @@
 export default {
-  "---": {
+  "getting-started-section": {
     title: "Getting Started",
     type: "separator",
   },
@@ -7,4 +7,14 @@ export default {
   index: "Introduction",
   setup: "Setup and configuration",
   cli: "CLI",
+  changelog: {
+    title: "Changelog",
+    href: "https://github.com/nomanr/lumo-ui/releases",
+    newWindow: true,
+  },
+
+  "components-section": {
+    title: "Components",
+    type: "separator",
+  },
 };

963ce2e36f20c2db117756e204e1e5051c70a1fe
nomanr <nomrafique@gmail.com>
2025-01-08 13:46:03 +0000

    add favicons

--- theme.config.tsx
+++ theme.config.tsx
@@ -48,18 +48,18 @@ const config: DocsThemeConfig = {
         <link
           rel="icon"
           type="image/png"
-          href="/favicon-96x96.png"
+          href="favicon/favicon-96x96.png"
           sizes="96x96"
         />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="favicon/favicon.svg" />
+        <link rel="shortcut icon" href="favicon/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="favicon/apple-touch-icon.png"
         />
         <meta name="apple-mobile-web-app-title" content="Lumo UI" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="favicon/site.webmanifest" />
       </>
     );
   },

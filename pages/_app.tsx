5dc27372d0bacf63e8e171a3292da88225396b09
nomanr <nomrafique@gmail.com>
2025-01-08 20:51:36 +0000

    update app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import "nextra-theme-docs/style.css";
 import "../styles.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }

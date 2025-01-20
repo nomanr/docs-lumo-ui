2fe38b87a41c7f89ca02d07d075d783b41d8825c
nomanr <nomrafique@gmail.com>
2025-01-20 10:36:21 +0000

    add vercel speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from "next/app";
-import Head from "next/head";
 import "nextra-theme-docs/style.css";
 import "../styles.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }

97d5603c0831a1d2aba17df0673daab7876f0543
nomanr <nomrafique@gmail.com>
2025-01-21 09:18:06 +0000

    clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "nextra-theme-docs/style.css";
 import "../styles.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="510c18cc-d810-4543-b266-5701963fce76"
+      />
     </>
   );
 }

1d2f84b9782505483b1d1cadd277366eb0c9a2e3
nomanr <nomrafique@gmail.com>
2025-02-03 17:17:50 +0000

    permanent redirect to lumoui.com

--- components/Umami.tsx
+++ components/Umami.tsx
@@ -6,11 +6,6 @@ const Umami = () => {
       <Script
         defer
         src="https://cloud.umami.is/script.js"
-        data-website-id="510c18cc-d810-4543-b266-5701963fce76"
-      />
-      <Script
-        defer
-        src="https://cloud.umami.is/script.js"
         data-website-id="e0246331-62a7-499b-8e87-7446c05285db"
       />
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,14 @@ import { AppProps } from "next/app";
 import "nextra-theme-docs/style.css";
 import "../styles.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Script from "next/script";
+import Umami from "@components/Umami";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <SpeedInsights />
-      <Script
-        defer
-        src="https://cloud.umami.is/script.js"
-        data-website-id="510c18cc-d810-4543-b266-5701963fce76"
-      />
+      <Umami />
     </>
   );
 }
